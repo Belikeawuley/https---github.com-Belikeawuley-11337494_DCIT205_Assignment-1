048628464c2c1e225b7048a4001be68f6c518a27
Belikeawuley <user.awuleygideon96@gmail.com>
2023-12-08 22:48:29 +0000

    Last Commit

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,11 +1,38 @@
+import user_icon from '../assets/person-fill.svg'
+import password_icon from '../assets/lock-fill.svg'
+import user_icon from '../assets/person-fill.svg'
+import password_icon from '../assets/lock-fill.svg'
 
 
 const Landing = ({category}) => {
+const Login = () => {
+ const [action,setAction] = useState("Login");
+    <i className="bi bi-person-fill"></i>
   return (
     <div>
-       <img src="..." class="img-thumbnail" alt="..."></img>
+    <div className='container'>
+      <div className="header">
+        <div className='text'>{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="ID Number" placeholder='ID' />
+        </div>
+        <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="Password" placeholder='PIN' />
+        </div>
+      </div>
+      <div className="forgot-password">Forgot Pin <span>Click Here!</span></div>
+      <div className="submit-container">
+        <div className={"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+      </div>
+    </div>
     </div>
   )
+}
 }
 
 export default Landing
